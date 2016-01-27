c80b73ab43d45af47baf9437d19eb84928a7ce0a
Matt Trask <mjftrask@gmail.com>
2016-01-26 19:41:12 -0500

    Update hello-http.js

--- hello-http.js
+++ hello-http.js
@@ -11,4 +11,4 @@ var server = http.createServer(function (request, response) {
 server.listen(8000);
 
 // Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:8000/");
+console.log('Server running at http://127.0.0.1:8000/');
